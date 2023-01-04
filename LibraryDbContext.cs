dcc06001c23102a11a089198ef30b3ed687cb752
Prasen Biswas <73785918+prasenbiswas@users.noreply.github.com>
2023-01-04 15:38:27 +0530

    Add files via upload

---
+++ LibraryDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication4.Data
+{
+    public class LibraryDbContext : IdentityDbContext
+    {
+        public LibraryDbContext(DbContextOptions<LibraryDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Seed();
+        }
+    }
+}
---
+++ SeedRoleData.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication4.Data
+{
+    public static class SeedRoleData
+    {
+        public static void Seed(this ModelBuilder builder)
+        {
+            var pwdAdmin = "Admin@123";
+            var pwdUser = "User@123";
+            var passwordHasher = new PasswordHasher<IdentityUser>();
+
+            // Seed Roles
+            var adminRole = new IdentityRole("Admin");
+            adminRole.NormalizedName = adminRole.Name.ToUpper();
+
+            var userRole = new IdentityRole("User");
+            userRole.NormalizedName = userRole.Name.ToUpper();
+
+            List<IdentityRole> roles = new List<IdentityRole>() {
+            adminRole,
+            userRole
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
+
+
+            // Seed Users
+            var adminUser = new IdentityUser
+            {
+                UserName = "Admin",
+                Email = "Admin@gmail.com",
+                EmailConfirmed = true,
+            };
+            adminUser.NormalizedUserName = adminUser.UserName.ToUpper();
+            adminUser.NormalizedEmail = adminUser.Email.ToUpper();
+            adminUser.PasswordHash = passwordHasher.HashPassword(adminUser, pwdAdmin);
+
+            var memberUser = new IdentityUser
+            {
+                UserName = "User",
+                Email = "User@gmail.com",
+                EmailConfirmed = true,
+            };
+            memberUser.NormalizedUserName = memberUser.UserName.ToUpper();
+            memberUser.NormalizedEmail = memberUser.Email.ToUpper();
+            memberUser.PasswordHash = passwordHasher.HashPassword(memberUser, pwdUser);
+
+            List<IdentityUser> users = new List<IdentityUser>() {
+                adminUser,
+                memberUser,
+            };
+
+            builder.Entity<IdentityUser>().HasData(users);
+
+            // Seed UserRoles
+            List<IdentityUserRole<string>> userRoles = new List<IdentityUserRole<string>>();
+
+            userRoles.Add(new IdentityUserRole<string>
+            {
+                UserId = users[0].Id,
+                RoleId = roles.First(q => q.Name == "Admin").Id
+            });
+
+            userRoles.Add(new IdentityUserRole<string>
+            {
+                UserId = users[1].Id,
+                RoleId = roles.First(q => q.Name == "User").Id
+            });
+
+
+            builder.Entity<IdentityUserRole<string>>().HasData(userRoles);
+        }
+    }
+}
